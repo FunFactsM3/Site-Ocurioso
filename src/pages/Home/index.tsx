a749087137ba0a598d813f0dd16d95d40cce8154
Bruno120Ab <abreubruno499@gmail.com>
2023-03-13 13:41:17 -0300

    fix: debug

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import { HeaderHome } from "../../components/HeaderHome";
 import { HomePageStyled } from "./style";
 import { SearchHome } from "../../components/SearchHome";
 import { MainHome } from "../../components/MainHome";
 import { FooterHome } from "../../components/FooterHome";
 import { ModalHome } from "../../components/ModalHome";
-import { UserContext } from "../../providers/UserContext";
 
 const DashBoardPage = () => {
 
